d73d14105493e570e6d918f78de0576ea455da13
SSSevaK <wispvsolo@gmail.com>
2025-09-14 23:09:24 +0300

    Update L1_n8.cpp

--- L1_n8.cpp
+++ L1_n8.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 
 int main()
@@ -10,7 +10,10 @@ int main()
     std::cin >> x1 >> y1;
     std::cin >> x2 >> y2;
     std::cin >> x3 >> y3;
-    std::cout << "perimeter of triangle = " << sqrt((x2 - x1)*(x2 - x1) + (y2 - y1) * (y2 - y1)) + sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2))+ sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
+    std::cout << "1 side of triangle = " << sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    std::cout << "2 side of triangle = " << sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
+    std::cout << "3  side of triangle = " << sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
+    std::cout << 0.5 * std::abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
     return 0;
 
 
